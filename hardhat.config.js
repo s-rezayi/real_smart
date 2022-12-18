8f184977ad5d14a4be6f8b737309b8be2c64019b
s-rezayi <rezayi.soheil@gmail.com>
2022-12-18 13:56:09 -0800

    implement deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,18 +10,18 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.17",
-    networks: {
-        hardhat: {},
-        goerli: {
-            url: GOERLI_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            chainId: 5,
-        },
-        localhost: {
-            url: "http://localhost:8545",
-            chainId: 31337,
-        },
-    },
+    // networks: {
+    //     hardhat: {},
+    //     goerli: {
+    //         url: GOERLI_RPC_URL,
+    //         accounts: [PRIVATE_KEY],
+    //         // chainId: 5,
+    //     },
+    //     localhost: {
+    //         url: "http://localhost:8545",
+    //         chainId: 31337,
+    //     },
+    // },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
     },
